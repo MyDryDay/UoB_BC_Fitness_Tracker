ebe12cec6c65a1eb352cfbc2a3bbc64b3b0ea076
George Cope <G.Cope13@gmail.com>
2021-04-20 16:53:46 +0100

    Added routes for the API - these need some more work

---
+++ routes/api/apiRoutes.js
@@ -0,0 +1,64 @@
+// Declare requirements
+const Workout = require('../../models/workoutModel');
+const mongojs = require('mongojs');
+const express = require('express');
+const router = require('express').Router();
+
+// POST route
+// Creates a new workout model & adds it to the DB
+router.post('/api/workouts', (req, res) => {
+    Workout.create({}).try(
+        (fitnessdb) => {
+            res.json(fitnessdb);
+        }
+    ).catch(
+        (err) => {
+            res.json(err);
+        }
+    );
+});
+
+// GET routes
+router.get('/api/workouts', (req, res) => {
+    Workout.find({}).try(
+        (fitnessdb) => {
+            res.json(fitnessdb)
+        }
+    ).catch(
+        (err) => {
+            res.json(err);
+        }
+    );
+});
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.find({}).limit(7).try(
+        (fitnessdb) => {
+            res.json(fitnessdb);
+        }
+    ).catch(
+        (err) => {
+            res.json(err);
+        }
+    );
+});
+
+// PUT route
+router.put('/api/workouts/:id', (req, res) => {
+    const id = mongojs.ObjectId(req.params.id);
+    Workout.findOneAndUpdate(
+        {_id: id},
+        {$push: {exercises: body}},
+        {new: true}
+    ).try(
+        (fitnessdb) => {
+            res.json(fitnessdb);
+        }
+    ).catch(
+        (err) => {
+            res.json(err);
+        }
+    );
+});
+
+module.exports = router;
