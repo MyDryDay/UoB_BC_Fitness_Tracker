ad54b723824c86b48d0f0e232d8b0aba32d9fd0a
George Cope <G.Cope13@gmail.com>
2021-04-20 13:00:53 +0100

    Added & tested server.js file, chnages will be made later

---
+++ server.js
@@ -0,0 +1,23 @@
+// Necessary dependencies & variables
+
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+// !!! UNCOMMENT ONCE USERMODEL.JS IS CREATED !!!
+// const User = require('./userModel.js');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnessdb', {useNewUrlParser:true});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}.`);
+});
