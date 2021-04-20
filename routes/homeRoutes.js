258ab8ee075e97ed121a432939ad3bd21dca20f2
George Cope <G.Cope13@gmail.com>
2021-04-20 16:51:12 +0100

    Created routes to get the index, exercise & stats HTML pages

---
+++ routes/homeRoutes.js
@@ -0,0 +1,22 @@
+// Include dependencies
+
+// path npm package allows directory paths to be shortened
+const path = require('path');
+const router = require('express').Router();
+
+// Retrieves the index.html file and displays it
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Retrieves the exercise.html file and displays it
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+// Retrieves the stats.html file and displays it
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+module.exports = router;
