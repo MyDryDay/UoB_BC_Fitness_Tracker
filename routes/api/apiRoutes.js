f08ed85c8a8a126e30662d9827704f1c7b7f8aa3
George Cope <G.Cope13@gmail.com>
2021-04-25 20:12:15 +0100

    Added comments, refactored & changed get all route to include forEach loops

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,15 +1,14 @@
 // Declare requirements
-const Workout = require('../../models/workoutModel');
+const db = require('../../models');
 const mongojs = require('mongojs');
-const express = require('express');
 const router = require('express').Router();
 
 // POST route
-// Creates a new workout model & adds it to the DB
-router.post('/api/workouts', (req, res) => {
-    Workout.create({}).try(
-        (fitnessdb) => {
-            res.json(fitnessdb);
+// Creates a new workout
+router.post('/api/workouts', ({body}, res) => {
+    db.Workout.create(body).then(
+        (dbWorkout) => {
+            res.json(dbWorkout);
         }
     ).catch(
         (err) => {
@@ -19,10 +18,20 @@ router.post('/api/workouts', (req, res) => {
 });
 
 // GET routes
+// Get all workouts
 router.get('/api/workouts', (req, res) => {
-    Workout.find({}).try(
-        (fitnessdb) => {
-            res.json(fitnessdb)
+    db.Workout.find({}).then(
+        (dbWorkout) => {
+            dbWorkout.forEach(workout => {
+                let total = 0;
+                workout.exercises.forEach(ex => {
+                    // total += ex.duration;
+                    total = total + ex.duration;
+                    console.log(total);
+                });
+                workout.totalDuration = total;
+            });
+            res.json(dbWorkout);
         }
     ).catch(
         (err) => {
@@ -31,10 +40,11 @@ router.get('/api/workouts', (req, res) => {
     );
 });
 
+// Get latest 7 workouts
 router.get('/api/workouts/range', (req, res) => {
-    Workout.find({}).limit(7).try(
-        (fitnessdb) => {
-            res.json(fitnessdb);
+    db.Workout.find({}).limit(7).then(
+        (dbWorkout) => {
+            res.json(dbWorkout);
         }
     ).catch(
         (err) => {
@@ -44,15 +54,20 @@ router.get('/api/workouts/range', (req, res) => {
 });
 
 // PUT route
+// Adds an exercise to a workout
 router.put('/api/workouts/:id', (req, res) => {
-    const id = mongojs.ObjectId(req.params.id);
-    Workout.findOneAndUpdate(
-        {_id: id},
-        {$push: {exercises: body}},
+    db.Workout.findOneAndUpdate(
+        // Filters records by id
+        {_id: req.params.id},
+        // Updates records with the totalDuration & exercises
+        {
+            $inc: {totalDuration: req.body.duration},
+            $push: {exercises: req.body}
+        },
         {new: true}
-    ).try(
-        (fitnessdb) => {
-            res.json(fitnessdb);
+    ).then(
+        (dbWorkout) => {
+            res.json(dbWorkout);
         }
     ).catch(
         (err) => {
