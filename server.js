b385a376eb6d8d0abc8b0e14b2c91019c89775a6
George Cope <G.Cope13@gmail.com>
2021-04-25 20:09:40 +0100

    Updated server.js to use mongo Atlas

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 // Necessary dependencies & variables
-
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 
-const PORT = process.env.PORT || 3000;
+// const routes = require('./routes');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {useNewUrlParser:true});
+// app.use(routes);
+app.use(require('./routes/homeRoutes'));
+app.use(require('./routes/api/apiRoutes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', 
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}.`);
